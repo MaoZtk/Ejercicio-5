a78840b802298fa323522b6c430cc54ed890bbb3
MaoZtk <oomarielos@gmail.com>
2023-10-16 15:55:51 -0600

    Catalogo completo
    podrían haber correcciones

--- Catalogo.java
+++ Catalogo.java
@@ -1,3 +1,103 @@
-public class Catalogo {
+//Universidad del Valle de Guatemala
+//Departamento de Ciencia de la Computación
+//Autor: Marielos Ortiz, Sandra Pineda
+//Carné: 23882, 231137
+//CC2008 - 50
+//Fecha: 16 de Octubre de 2023
+//Descripción: Ejercicio 5 en parejas sobre un programa de creación de citas y referencias APA 7.
+//Esta clase es del contenido del catálogo existente de referencias y citas, en conjunto con los conteos necesarios en métodos.
+//************************************************************************
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Esta clase representa un catálogo de materiales bibliográficos que extiende la clase MaterialBibliografico.
+ * Un catálogo es una colección de materiales bibliográficos con funcionalidades para contar y generar citas APA.
+ */
+public class Catalogo extends MaterialBibliografico {
+    private List<Material_Bibliografico> materiales;
     
+    /**
+     * Constructor para crear un catálogo con información básica.
+     *
+     * @param nombre           El nombre del catálogo.
+     * @param editorial        La editorial del catálogo.
+     * @param anioPublicacion  El año de publicación del catálogo.
+     * @param genero           El género o categoría del catálogo.
+     * @param autor            El autor del catálogo.
+     */
+    public Catalogo(String nombre, String editorial, int anioPublicacion, String genero, String autor){
+        super (nombre, editorial, anioPublicacion, genero, autor);
+        materiales = new ArrayList<>();
+    } 
+
+    /**
+     * Agrega un material bibliográfico al catálogo.
+     *
+     * @param material El material bibliográfico a agregar.
+     */
+    public void agregarMaterial(Material_Bibliografico material) {
+        materiales.add(material);
+    }
+
+    /**
+     * Cuenta la cantidad de materiales por género.
+     *
+     * @return Un mapa que asocia géneros con la cantidad de materiales en el catálogo de ese género.
+     */
+    public Map<String, Integer> contarPorGenero() {
+        Map<String, Integer> conteoGenero = new HashMap<>();
+
+        for (Material_Bibliografico material : materiales) {
+            String genero = material.getGenero();
+            conteoGenero.put(genero, conteoGenero.getOrDefault(genero, 0) + 1);
+        }
+
+        return conteoGenero;
+    }
+
+    /**
+     * Cuenta la cantidad de materiales por autor.
+     *
+     * @return Un mapa que asocia autores con la cantidad de materiales en el catálogo de ese autor.
+     */
+    public Map<String, Integer> contarPorAutor() {
+        Map<String, Integer> conteoAutor = new HashMap<>();
+
+        for (Material_Bibliografico material : materiales) {
+            String autor = material.getAutor();
+            conteoAutor.put(autor, conteoAutor.getOrDefault(autor, 0) + 1);
+        }
+
+        return conteoAutor;
+    }
+
+    /**
+     * Cuenta la cantidad de materiales por año de publicación.
+     *
+     * @return Un mapa que asocia años de publicación con la cantidad de materiales en el catálogo de ese año.
+     */
+    public Map<Integer, Integer> contarPorAño() {
+        Map<Integer, Integer> conteoAño = new HashMap<>();
+
+        for (Material_Bibliografico material : materiales) {
+            int año = material.getAñoPublicacion();
+            conteoAño.put(año, conteoAño.getOrDefault(año, 0) + 1);
+        }
+
+        return conteoAño;
+    }
+
+    /**
+     * Genera citas APA para todos los materiales del catálogo y las muestra en la consola.
+     */
+    public void generarCatalogo() {
+        for (Material_Bibliografico material : materiales) {
+            String citaAPA = Generador_APA.generarCitaAPA(material);
+            System.out.println(citaAPA);
+        }
+    }
 }
